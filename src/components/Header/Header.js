2cd435c280888e9e9b9bea16460714102a95e7f6
VO DUY QUANG <voduyquang123>
2025-08-23 23:53:02 +0700

    🐛 fix: sửa lỗi build do ký tự '/' trong CSS

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
+import React, { useEffect, useState } from "react";
 import "./Header.css";
-import "./HeaderResponsive.css";
-
+import "./HeaderResponsive.css"; // 👈 responsive tách riêng
 import logo from "../../assets/logo.png";
 import accountIcon from "../../assets/account.png";
 import cartIcon from "../../assets/shopping-cart.png";
@@ -8,9 +8,16 @@ import dropdownIcon from "../../assets/Dropdown.png";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const [open, setOpen] = useState(false);
+
+  // khoá scroll khi mở menu
+  useEffect(() => {
+    document.body.classList.toggle("no-scroll", open);
+    return () => document.body.classList.remove("no-scroll");
+  }, [open]);
+
   return (
     <header className="navbar">
-      {/* Logo + menu giống nhau */}
       <div className="container">
         <div className="row align-center space-between">
           <div className="col col-logo">
@@ -18,6 +25,8 @@ function Header() {
               <img src={logo} alt="Logo" className="logo-img" />
             </Link>
           </div>
+
+          {/* MENU DESKTOP */}
           <div className="col col-menu">
             <nav className="menu">
               <ul className="row menu-list">
@@ -40,7 +49,7 @@ function Header() {
             </nav>
           </div>
 
-          {/* Phần icon hồ sơ + giỏ hàng */}
+          {/* ICONS DESKTOP */}
           <div className="col col-icons">
             <div className="row icons">
               <div className="icon-account">
@@ -55,16 +64,112 @@ function Header() {
                 </Link>
               </div>
               <div className="icon-wrapper dropdown">
-                <a href="#" title="Ngôn ngữ">
+                <button type="button" title="Ngôn ngữ">
                   <img src={dropdownIcon} alt="Dropdown" />
-                </a>
+                </button>
               </div>
             </div>
           </div>
+
+          {/* HAMBURGER (mobile) */}
+          <button
+            className="hamburger"
+            aria-label="Mở menu"
+            aria-expanded={open}
+            aria-controls="mobile-menu"
+            onClick={() => setOpen(true)}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
         </div>
       </div>
+
+      {/* DRAWER MOBILE */}
+      <div
+        id="mobile-menu"
+        className={`mobile-menu ${open ? "open" : ""}`}
+        role="dialog"
+        aria-modal="true"
+      >
+        <div className="mobile-header">
+          <img src={logo} alt="Logo" className="mobile-logo" />
+          <button
+            className="mobile-close"
+            aria-label="Đóng menu"
+            onClick={() => setOpen(false)}
+          >
+            ×
+          </button>
+        </div>
+
+        <nav className="mobile-nav">
+          <Link
+            to="/"
+            className="mobile-link active"
+            onClick={() => setOpen(false)}
+          >
+            Trang chủ
+          </Link>
+          <Link
+            to="/about"
+            className="mobile-link"
+            onClick={() => setOpen(false)}
+          >
+            Giới thiệu
+          </Link>
+          <Link
+            to="/product"
+            className="mobile-link"
+            onClick={() => setOpen(false)}
+          >
+            Sản phẩm
+          </Link>
+          <Link
+            to="/history"
+            className="mobile-link"
+            onClick={() => setOpen(false)}
+          >
+            Lịch sử
+          </Link>
+          <Link
+            to="/contact"
+            className="mobile-link"
+            onClick={() => setOpen(false)}
+          >
+            Liên hệ
+          </Link>
+        </nav>
+
+        <div className="mobile-lang">
+          <span className="flag">🇻🇳</span>
+          <span>VN</span>
+          <span className="caret">▾</span>
+        </div>
+
+        <div className="mobile-actions">
+          <Link
+            to="/login"
+            className="btn-login"
+            onClick={() => setOpen(false)}
+          >
+            Đăng nhập
+          </Link>
+          <Link
+            to="/register"
+            className="txt-register"
+            onClick={() => setOpen(false)}
+          >
+            Đăng ký
+          </Link>
+        </div>
+      </div>
+
+      {open && (
+        <div className="mobile-backdrop" onClick={() => setOpen(false)} />
+      )}
     </header>
   );
 }
-
 export default Header;
